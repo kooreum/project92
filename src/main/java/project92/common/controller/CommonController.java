8b4f6c09513f0e82857041df84b3871bdac18855
hagjoon <gkrwns6307@gmail.com>
2024-01-04 23:55:44 +0900

    초기화

--- src/main/java/project92/common/controller/CommonController.java
+++ src/main/java/project92/common/controller/CommonController.java
@@ -1,12 +1,8 @@
 package project92.common.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Collection;
 
 
 @Controller
@@ -15,20 +11,7 @@ public class CommonController {
 
 	@GetMapping("/")
 	public String redirectToDashboard() {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-		for (GrantedAuthority grantedAuthority : authorities) {
-			String role = grantedAuthority.getAuthority();
-			switch (role) {
-				case "ROLE_ADMIN":
-					return "redirect:/admin";
-				case "ROLE_MANAGER":
-					return "redirect:/manager";
-				case "ROLE_SUPPLIER":
-					return "redirect:/supplier";
-			}
-		}
-		return "redirect:/";
+		return "layout/default";
 	}
 
 }
